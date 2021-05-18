b6d0f4f024949879d0d2e81e2068a5bc648b1949
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2021-05-18 13:30:28 +0300

    Mesto

--- script/script.js
+++ script/script.js
@@ -1,12 +1,11 @@
 let addPopup = document.querySelector('.popup');
-let profile = document.querySelector('.profile');
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__job');
 let nameInput = document.querySelector('.form__item_name');
 let jobInput = document.querySelector('.form__item_job');
 let openPopup = document.querySelector('.profile__button_edit');
 let closePopup = document.querySelector('.form__toogle');
-let formButton = document.querySelector('.form__button')
+let formData = document.querySelector('.form__data');
 
 function popupOpen() {
     addPopup.classList.add('popup_opened');
@@ -16,8 +15,9 @@ function popupOpen() {
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    nameInput.value = nameProfile.textContent;
-    jobInput.value = jobProfile.textContent;
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value;
+    addPopup.classList.remove('popup_opened');
 }
 
 function popupClose() {
@@ -28,4 +28,4 @@ openPopup.addEventListener('click', popupOpen);
 
 closePopup.addEventListener('click', popupClose);
 
-formButton.addEventListener('submit', formSubmitHandler);
+formData.addEventListener('submit', formSubmitHandler);
