aef996ca869732c2a94c88b666463f9247efef04
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2021-05-26 23:33:29 +0300

    fix: исправить структуру карточек

--- script/script.js
+++ script/script.js
@@ -1,11 +1,26 @@
-let addPopup = document.querySelector('.popup');
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__job');
-let nameInput = document.querySelector('.form__input_js_name');
-let jobInput = document.querySelector('.form__input_js_job');
-let openPopup = document.querySelector('.profile__button-edit');
-let closePopup = document.querySelector('.popup__toggle');
-let formData = document.querySelector('.form__data');
+const addPopup = document.querySelector('.popup');
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__job');
+const nameInput = document.querySelector('.form__input_js_name');
+const jobInput = document.querySelector('.form__input_js_job');
+const openPopup = document.querySelector('.profile__button-edit');
+const closePopup = document.querySelector('.popup__toggle');
+const formData = document.querySelector('.form__data');
+const cardsList = document.querySelector('.cards__list');
+const card = document.querySelector('.card');
+
+// const userTemplate = document.querySelector('.cards__list').content;
+// const usersOnline = document.querySelector('.cards');
+
+// // клонируем содержимое тега template
+// const userElement = userTemplate.querySelector('.card').cloneNode(true);
+
+// // наполняем содержимым
+// userElement.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+// userElement.querySelector('.card__title').textContent = 'Архыз';
+
+// // отображаем на странице
+// usersOnline.append(userElement);
 
 function popupOpen() {
     addPopup.classList.add('popup_opened');
